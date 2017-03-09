15d7c20869344f733bf9d27036fff07c0c47b6a6
Jeff Silberman <jsilberm@gmail.com>
2017-03-09 12:47:49 -0800

    Update Provider.tf

--- azure/coreos/Provider.tf
+++ azure/coreos/Provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  subscription_id = "72c299a4-a431-4b8e-80ef-6855109979d9"
-  client_id       = "37660f27-86da-4e85-8889-dd43e9507d9d"
-  client_secret   = "oMszZZH87fmYepBmneM2CFmUxPOrCxtyZB3FcAVqcSs="
-  tenant_id       = "ca9700ce-d438-4389-b86f-09c48f71d0ce"
+  subscription_id = "..."
+  client_id       = "..."
+  client_secret   = "..."
+  tenant_id       = "..."
 }
