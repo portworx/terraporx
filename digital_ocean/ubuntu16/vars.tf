0bf2e3eeb462d03b20a55ff2e97691fb9df9c941
Jeff Silberman <jsilberm@gmail.com>
2018-04-05 10:38:01 -0700

    Updated for runc and get.portworx.com

--- digital_ocean/ubuntu16/vars.tf
+++ digital_ocean/ubuntu16/vars.tf
@@ -1,38 +1,46 @@
 variable "do_token" {
   description = "API Token"
+  default = ""
 }
 
 variable "do_count" {
-  description = "# instannces"
+  description = "# instances"
   default = 3
 }
 
 variable "region" {
-   description = "Block storage only available in fra1, nyc1, sfo2 and sgp2"
+   description = "Block storage only available in fra1, nyc1, sfo2 and sgp1"
+   default = "sfo2"
 }
 
 variable "size" {
    description = "Instance size: [ 2gb, 4gb, 8gb, 16gb, 32gb, 48gb, 64gb ]"
+   default = "4gb"
 }
 
 variable "volsize" {
    description = "Volume size : [ 100, 250, 500, 1000, 2000 ]"
+   default = 100
 }
 
 variable "prefix" {
    description = "Prefix string for volumes and instances"
+   default = "my"
 }
 
 variable "ssh_key_path" {
   description = "private key path"
+  default = "~/.ssh/id_rsa"
 }
 
 variable "pub_key" {
   description = "ssh_key"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "ssh_fingerprint" {
   description = "fingerprint"
+  default = ""
 }
 
 
